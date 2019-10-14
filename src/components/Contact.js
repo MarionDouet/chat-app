b53781a90bc14e4f8cc0932fa4ff66473f7d7c46
Marion Douet <squalaladraw@gmail.com>
2019-10-14 13:54:33 +0200

    ajout contact avec state

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,21 +1,29 @@
 import React from 'react';
 import './Contact.css';
 
-function Contact(props){
-    return( 
-        <div className="Contact">
-            <img class="avatar" src={props.avatar}></img>
-            <div>
-            <h4 className="name">{props.name}</h4>
-            <div className="status">
-                <p className = "status-text">{props.online ? "Online" : "Offline"}</p>
-                <span className= {props.online ? "status-online" : "status-offline"}></span>
-                
-               
-            </div> 
-        </div>    
-        </div>   
-    )
+class Contact extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            online: this.props.online
+        };
+    }
+    render(){ 
+        return <div className="Contact">
+                    <img className="avatar" src={this.props.avatar}></img>
+                    <div>
+                        <h4 className="name">{this.props.name}</h4>
+                            <div className="status"
+                                onClick={() => {
+                                    const newOnline = ! this.state.online;
+                                    this.setState({online: newOnline});
+                                }}>
+                                 <p className = "status-text">{this.state.online ? "Online" : "Offline"}</p>
+                                 <span className= {this.state.online ? "status-online" : "status-offline"}></span>       
+                            </div> 
+                    </div>    
+                </div>   
+    }
   }
 
 export default Contact;
